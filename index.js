ba9a04975f4dcf51f09989dbc6959c29c72544d9
Devon Wesley <devonwesley@Devons-MacBook-Pro.local>
2018-02-27 15:00:24 -0800

    Took out ethereumjs-util, not needed

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const keythereum = require('keythereum');
-const ethUtil = require('ethereumjs-util');
 const qr = require('qr-image');
 const pictureTube = require('picture-tube');
 
@@ -7,20 +6,18 @@ const tube = pictureTube();
 tube.pipe(process.stdout);
 
 const privateKeyBuffer = keythereum.create().privateKey;
-const addressBuffer = ethUtil.privateToAddress(privateKeyBuffer);
-
+const addressKey = keythereum.privateKeyToAddress(privateKeyBuffer);
 const privateKey = `0x${privateKeyBuffer.toString('hex')}`;
-const address = `0x${addressBuffer.toString('hex')}`;
 
 process.stdout.write(
 `
 Generated Keypair
 -----------------
 
-Address: ${address}
+Address: ${addressKey}
 Private_Key: ${privateKey}
 
 `
 );
 
-qr.image(address, {type: 'png', size: 1, margin: 2,}).pipe(tube);
+qr.image(addressKey, {type: 'png', size: 1, margin: 2,}).pipe(tube);
